43077b18d33e164e46c1e84a1752b3cb8f4a0f08
JoeHowarth <JosepheHowarth@gmail.com>
2017-11-29 22:44:06 -0500

    more frontend work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,30 +7,45 @@ import Sentence from './components/sentence.js';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       sents: sents,
+      correct: 0,
       human: 0,
       machine: 0
     }
-
-
+    this.checkCorrect = this.checkCorrect.bind(this);
     // this.handleKey = this.handleKey.bind(this);
     // this.reset = this.reset.bind(this);
     // window.addEventListener('keydown', this.handleKey);
   }
 
-  checkCorrect(label, id) {
+  checkCorrect(sent, id, child) {
     // if sentence human and clicked, add to correct
-    if (label == 1) {
-      this.state.correct += 1
+    let clicked = child.state.clicked
+    const label = sent.label
+    if (!clicked && label === "1") {
+      this.setState({
+        correct: this.state.correct + 1
+      })
+      console.log(sent);f
     }
+    child.setState({
+      clicked: true
+    })
+
   }
 
   render() {
     let components = sents.map((s, ind, sents) => {
-        return <Sentence sent={s} key={ind} onClick=></Sentence>
+        return (
+          <Sentence
+            sent={s}
+            key={ind}
+            checkCorrect={this.checkCorrect}>
+          </Sentence>
+        )
     })
+
 
 
     return (
@@ -40,7 +55,8 @@ class App extends Component {
           <h1 className="App-title">Meta Turing Test!</h1>
         </header>
         {components}
-        <Sentence sent={sents[0]}></Sentence>
+        {/* <Sentence sent={sents[0]}></Sentence> */}
+        <div>{this.state.correct}</div>
       </div>
     );
   }
--- frontend/src/components/sentence.js
+++ frontend/src/components/sentence.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Sentence extends Component {
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      human_correct: null,
+      mach_correct: null,
+      clicked: false
+    }
+  }
 
   render() {
     const sent = this.props.sent
-
+    const id = this.props.id
 
     return (
-      <div className={"Sentence "+sent.class}>
+      <div
+        className={"Sentence "+sent.class}
+        onClick={() => this.props.checkCorrect(sent, id, this)}
+      >
         <p className="sentText" >{sent.text}</p>
         <p className="sentClass" >{sent.class}</p>
       </div>
