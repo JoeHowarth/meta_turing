79c254e69c78a4f767ecd17e1b7f4b60c3054a41
Joseph Howarth <jh@Josephs-MacBook-Pro.local>
2017-12-04 18:29:12 -0500

    frontend stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import sents from './data/sents.json';
 import Sentence from './components/sentence.js';
@@ -7,15 +6,17 @@ import Sentence from './components/sentence.js';
 class App extends Component {
   constructor(props) {
     super(props);
-    machine_corr = sents.reduce((total, s) => total + s.machine_corr, 0)
+    const machine_corr = sents.reduce((total, s) => s.machine_corr? total + 1: total, 0)
     this.state = {
       mach_acc: machine_corr,
       sents: sents,
       correct: 0,
       human: 0,
-      machine: 0
+      machine: 0,
+      display: false,
     }
     this.checkCorrect = this.checkCorrect.bind(this);
+    this.onDone = this.onDone.bind(this);
     // this.handleKey = this.handleKey.bind(this);
     // this.reset = this.reset.bind(this);
     // window.addEventListener('keydown', this.handleKey);
@@ -34,7 +35,13 @@ class App extends Component {
     child.setState({
       clicked: true
     })
+  }
 
+  onDone() {
+    this.setState({
+      display: 'answers',
+    })
+    console.log("hi");
   }
 
   render() {
@@ -47,25 +54,54 @@ class App extends Component {
           </Sentence>
         )
     })
-
+    const state = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Meta Turing Test!</h1>
         </header>
         <div className="main_container">
           <div className="left_container">
             {components}
           </div>
-          <div className="info_container">
-            <div>Your correct choices: {this.state.correct}</div>
-            <div>Computer's correct choices: {this.state.mach_acc}</div>
-          </div>
+          <InfoContainer
+            correct={state.correct}
+            length={sents.length}
+            display={state.display}
+            mach_acc={state.mach_acc}
+            onDone={this.onDone}
+          />
         </div>
       </div>
     );
   }
+}
+
+
+
+const InfoContainer = (props) => {
+  let {correct, length, display, mach_acc} = props;
+  if (display === 'answers') {
+    return (
+      <div className="info_container">
+        <div>Your correct choices: {correct + " / " + length}</div>
+        <div>Computer's correct choices: {mach_acc + " / " + length}</div>
+      </div>
+    );
+  }
+  else  {
+    return (
+      <div className="info_container">
+        <button onClick={props.onDone}> Done </button>
+        <div>5 of the sentences are machine generated and 5 are human</div>
+        <div>Click on the sentences you think are human</div>
+        <div>Another neural network is given the same choice</div>
+        <h3>Are you better than the machine??</h3>
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
